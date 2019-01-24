9d70fe531171f446b502a499991497a6effadbdd
Kyoungsoo Park <36439380+KyoungsooPark@users.noreply.github.com>
2019-01-24 15:50:35 +0900

    Update 이동하기.cpp

--- 이동하기.cpp
+++ 이동하기.cpp
@@ -3,7 +3,6 @@ https://www.acmicpc.net/submit/11048
 */
 
 #include <cstdio>
-#include <cstring>
 using namespace std;
 
 int map[1001][1001];
@@ -13,15 +12,16 @@ int max(int a, int b) { return a >= b ? a : b; }
 
 int main(void) {
 	int N, M;
+	// 입력부
 	scanf("%d %d", &N, &M);
 	for (int i = 1; i <= N; i++)
 		for (int j = 1; j <= M; j++)
 			scanf("%d", &map[i][j]);
-
+	// 처리부
 	for (int i = 1; i <= N; i++)
 		for (int j = 1; j <= M; j++)
 			d[i][j] = max(d[i - 1][j], d[i][j - 1]) + map[i][j];
-
+	// 출력부
 	printf("%d\n", d[N][M]);
 	return 0;
 }
