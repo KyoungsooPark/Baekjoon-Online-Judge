893a7a2953eb31924fc0e377d5c94dec92c6c0f1
Kyoungsoo Park <36439380+KyoungsooPark@users.noreply.github.com>
2019-03-05 00:51:42 +0900

    Update 로봇 청소기.cpp

--- 로봇 청소기.cpp
+++ 로봇 청소기.cpp
@@ -5,41 +5,44 @@ https://www.acmicpc.net/problem/14503
 #include <cstdio>
 using namespace std;
 
-bool map[50][50], visited[50][50];
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int N, M, ans;
+bool map[50][50], check[50][50];
+int dx[4] = { -1, 0, 1, 0 };	// 북, 동, 남, 서
+int dy[4] = { 0, 1, 0, -1 };	// 북, 동, 남, 서
+int N, M, ans = 0;
 
 void go(int x, int y, int d) {
-	if (!visited[x][y]) {	// 현재 위치 청소 안했으면 청소
-		ans++;
-		visited[x][y] = true;
+	if (!check[x][y]) {	// 청소하지 않은 경우
+		check[x][y] = true;	// 청소
+		ans++;	// 청소한 칸 수 증가
 	}
-	
-	for (int i = 0; i < 4; i++) {	// 현재 방향을 기준으로 왼쪽 탐색
-		d == 0 ? d = 3 : d--;
-		int nx = x + dx[d], ny = y + dy[d];
-		if (!map[nx][ny] && !visited[nx][ny]) {	// 청소하지 않은 공간 존재
-			go(nx, ny, d);	// 해당 위치로 이동
+
+	int nx, ny, nd = d;
+	for (int i = 0; i < 4; i++) {
+		nd == 0 ? nd = 3 : nd--;	// 왼쪽으로 회전
+		nx = x + dx[nd], ny = y + dy[nd];	// 회전
+		if (!map[nx][ny] && !check[nx][ny]) {	// 벽이 없고 청소하지 않은 경우
+			go(nx, ny, nd);	// 해당 위치로 이동
 			return;
 		}
 	}
-	int nd = (d + 2) % 4;
-	int nx = x + dx[nd], ny = y + dy[nd];
-	if (!map[nx][ny])	// 뒤쪽 방향이 벽이 아니면
-		go(nx, ny, d);	// 후진
+	nd = (d + 2) % 4;	// 현재 방향의 반대 방향
+	nx = x + dx[nd], ny = y + dy[nd];	// 현재 위치의 뒷 칸
+	if (!map[nx][ny])	// 뒤에 벽이 없으면
+		go(nx, ny, d);	// 한 칸 뒤로 이동, 방향은 유지
 }
 
 int main(void) {
+	int r, c, d;
 	// 입력부
-	int x, y, d;
 	scanf("%d %d", &N, &M);
-	scanf("%d %d %d", &x, &y, &d);
+	scanf("%d %d %d", &r, &c, &d);
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < M; j++)
 			scanf("%d", &map[i][j]);
+	
 	// 처리부
-	go(x, y, d);
+	go(r, c, d);
+
 	// 출력부
 	printf("%d\n", ans);
 	return 0;
