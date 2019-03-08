f10163d7a6931a2c787a05437eba680ecb6a0af6
KyoungsooPark <ks08_18@hufs.ac.kr>
2019-03-08 19:35:16 +0900

    Update 나무 재테크.cpp

--- 나무 재테크.cpp
+++ 나무 재테크.cpp
@@ -4,96 +4,77 @@ https://www.acmicpc.net/problem/16235
 
 #include <cstdio>
 #include <queue>
-#define PRIORITY	1000
+#define MAX_AGE	1010
 using namespace std;
-typedef struct { int x, y, l; } point;
+typedef struct { int x, y; } tree;
 
-queue<point> q[PRIORITY];	// 우선순위 큐
-int map[10][10], A[10][10];
+queue<tree> q[MAX_AGE + 1];
+int map[10][10], A[10][10], dead[10][10];
 int dx[8] = { -1, -1, -1, 0, 0, 1, 1, 1 };
 int dy[8] = { -1, 0, 1, -1, 1, -1, 0, 1 };
 int N, M, K;
-
+// 봄: 나이가 어린 나무부터 자신의 나이만큼 양분을 먹고 나이 1 증가. 양분이 부족한 나무는 죽음
 void spring(void) {
-	int qsize[PRIORITY];
-	for (int p = 1; p < PRIORITY; p++)
-		// 나무의 나이를 증가시켜서 다시 큐에 삽입하므로 큐 사이즈가 수시로 달라짐
-		// 따라서 미리 현재 큐 사이즈를 저장해야 함
-		qsize[p] = q[p].size();
+	int qsize[MAX_AGE + 1];	// 나이 별 현재까지 존재하는 나무의 수
+	for (int age = 1; age <= MAX_AGE; age++)
+		qsize[age] = q[age].size();
 
-	for (int p = 1; p < PRIORITY; p++) {
-		while (qsize[p]--) {
-			point pnt = q[p].front();
-			q[p].pop();
-			if (map[pnt.x][pnt.y] >= p) {	// 양분이 충분한 경우
-				map[pnt.x][pnt.y] -= p;	// 양분 섭취
-				pnt.l++;		// 나이 증가
-				q[pnt.l].push(pnt);	// 해당 나이의 큐에 삽입
+	for (int age = 1; age <= MAX_AGE; age++) {
+		while (qsize[age]--) {
+			tree t = q[age].front(); q[age].pop();
+			if (map[t.x][t.y] < age) {	// 양분이 부족한 경우
+				dead[t.x][t.y] += age / 2;	// 여름에 추가될 양분을 dead 배열에 저장
+				continue;
 			}
-			else			// 양분이 부족한 경우
-				q[0].push(pnt);	// 나무가 죽어 0번째 큐에 삽입
+			map[t.x][t.y] -= age;	// map에서 나무의 나이만큼 양분 흡수
+			q[age + 1].push(t);	// 나이 1 증가
 		}
 	}
 }
-
+// 여름: 봄에 죽은 나무가 자신이 있던 칸에 나이를 2로 나눈 값만틈 양분으로 추가. 소수점 아래는 제거
 void summer(void) {
-	int qsize = q[0].size();
-	while (qsize--) {
-		point pnt = q[0].front();
-		q[0].pop();
-		map[pnt.x][pnt.y] += pnt.l / 2;	// 죽은 나무 나이의 절반이 양분으로 추가
-	}
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			map[i][j] += dead[i][j], dead[i][j] = 0;
 }
-
+// 가을: 나이가 5의 배수인 나무가 인접한 8개의 칸에 나이 1인 나무를 번식
 void fall(void) {
-	// 나이가 5의 배수인 나무가 번식
-	for (int p = 5; p < PRIORITY; p += 5) {
-		int qsize = q[p].size();
-		while (qsize--) {
-			point pnt = q[p].front();
-			q[p].pop();
-			for (int d = 0; d < 8; d++) {
-				int nx = pnt.x + dx[d], ny = pnt.y + dy[d];
+	for (int p = 5; p <= MAX_AGE; p += 5) {	// 나이가 5의 배수인 나무가
+		int size = q[p].size();
+		while (size--) {
+			tree t = q[p].front(); q[p].pop(); q[p].push(t);
+			for (int d = 0; d < 8; d++) {	// 인접한 8개의 칸에
+				int nx = t.x + dx[d], ny = t.y + dy[d];
 				if (0 <= nx && nx < N && 0 <= ny && ny < N)
-					q[1].push({ nx, ny, 1 });	// 번식 가능한 위치에 나이 1의 나무 번식
+					q[1].push({ nx, ny });	// 나이가 1인 나무를 번식
 			}
-			q[p].push(pnt);	// 새로운 나무를 번식시킨 나무를 다시 큐에 삽입
 		}
 	}
 }
-
+// 겨울: A에 저장된 양만큼 map에 양분 추가
 void winter(void) {
-	// 양분 추가
-	for (int x = 0; x < N; x++)
-		for (int y = 0; y < N; y++)
-			map[x][y] += A[x][y];
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			map[i][j] += A[i][j];
 }
 
 int main(void) {
 	int ans = 0;
-	
 	// 입력부
 	scanf("%d %d %d", &N, &M, &K);
-	for (int x = 0; x < N; x++) {
-		for (int y = 0; y < N; y++) {
-			scanf("%d", &A[x][y]);
-			map[x][y] = 5;
-		}
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			scanf("%d", &A[i][j]), map[i][j] = 5;
+	while (M--) {
+		int x, y, z;
+		scanf("%d %d %d", &x, &y, &z);
+		q[z].push({ x - 1, y - 1 });
 	}
-	for (int m = 0; m < M; m++) {
-		int x, y, l;
-		scanf("%d %d %d", &x, &y, &l);
-		q[l].push({ x - 1, y - 1, l });
-	}
-	
 	// 처리부
-	while (K--)
-		spring(), summer(), fall(), winter();
-	
+	while (K--) { spring(); summer(); fall(); winter(); }
+	for (int age = 1; age <= MAX_AGE; age++)
+		ans += q[age].size();
 	// 출력부
-	for (int p = 1; p < PRIORITY; p++)
-		ans += q[p].size();
 	printf("%d\n", ans);
-
 	return 0;
 }
