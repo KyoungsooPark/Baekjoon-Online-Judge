ac0a242e9d23a2326ef1cd0527ae629288c73e68
Kyoungsoo Park <36439380+KyoungsooPark@users.noreply.github.com>
2019-07-18 16:57:08 +0900

    Update 타임머신 (Bellman-Ford).cpp

--- 타임머신 (Bellman-Ford).cpp
+++ 타임머신 (Bellman-Ford).cpp
@@ -12,15 +12,27 @@ vector<Edge> v[501];
 int dist[501];
 int N, M;
 
-bool bellman_ford(bool check) {
-	for (int i = 1; i <= N; i++)
-		for (int j = 1; j <= N; j++)
-			for (const Edge e : v[j])
-				if (dist[j] != INF && dist[e.to] > dist[j] + e.cost) {
-					if (check)	return true;
-					dist[e.to] = dist[j] + e.cost;
-				}
+bool edge_relaxation(bool check) {
+	// 모든 노드에 대해
+	for (int j = 1; j <= N; j++)
+		// 해당 노드와 연결된 모든 간선
+		for (const Edge e : v[j])
+			// 가중치 업데이트 가능 여부 검사
+			if (dist[j] != INF && dist[e.to] > dist[j] + e.cost) {
+				// 음수 사이클 체크인 경우
+				if (check)	return true;
+				// 가중치 업데이트
+				dist[e.to] = dist[j] + e.cost;
+			}
 	return false;
+}
+
+bool bellman_ford(void) {
+	// 노드 개수만큼 edge_relaxation 수행
+	for (int i = 1; i <= N; i++)
+		edge_relaxation(false);
+	// 다시 한번 edge_relaxation 수행하여 음수 사이클 여부 검사
+	return edge_relaxation(true);
 }
 
 int main(void) {
@@ -30,12 +42,12 @@ int main(void) {
 		scanf("%d %d %d", &A, &B, &C);
 		v[A].push_back({ B, C });
 	}
-	for (int i = 2; i <= N; i++)	dist[i] = INF;
-	bellman_ford(false);
-	if (bellman_ford(true))	printf("-1\n");
-	else {
+	for (int i = 2; i <= N; i++)
+		dist[i] = INF;
+	if (bellman_ford())
+		printf("-1\n");
+	else
 		for (int i = 2; i <= N; i++)
 			printf("%d\n", dist[i] == INF ? -1 : dist[i]);
-	}
 	return 0;
 }
